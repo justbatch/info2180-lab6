4fc6c8d3b906bf150ed36a37c2981ece3dde10d1
Kevaughn Batchelor <kevaughn.batchelor@mymona.uwi.edu>
2016-11-12 04:23:02 +0000

    added updated files

--- javascript.js
+++ javascript.js
@@ -7,7 +7,8 @@ window.onload = function()
                 event.preventDefault();
                 var getHTTP = new XMLHttpRequest();
                 getHTTP.onreadystatechange = getHTML;
-                getHTTP.open("Get", "request.php?q=definition");
+                var url = "request.php?q="+document.getElementById("one").value;
+                getHTTP.open("Get", url);
                 getHTTP.send();
                 
                 function getHTML()
@@ -16,10 +17,47 @@ window.onload = function()
                     {
                         if(getHTTP.status === 200)
                         {
-                            alert(getHTTP.responseText);
+                            var def = getHTTP.responseText;
+                            // console.log(def);
+                            document.getElementById("result").innerHTML = def;
                         }
                     }
                 }
-            });
+            });    
+            
+                var three = document.getElementById("three");
+                    three.addEventListener("click", function(event)
+                    {
+                        event.preventDefault();
+                        //calling for XML data in php
+                        var getXMLdata = new XMLHttpRequest();
+                        getXMLdata.onreadystatechange = getXML;
+                        var link = "request?q=&all=true"+document.getElementById("three").value;
+                        getXMLdata.open("Get", link);
+                        getXMLdata.send();
+                        
+                        function getXML()
+                        {
+         
+                            if(getXMLdata.readyState === XMLHttpRequest.DONE)
+                            {
+                                if(getXMLdata.status === 200)
+                                {
+                                    var define = getXMLdata.responseXML;
+                                    var result = document.getElementById("result").innerHTML = define;
+                                    var r = result.querySelector("#result");
+                                    var dictionary = define.getElementsByTagName("dictionary");
+                                    var lis = document.createElement("ol");
+                                    r.appendChild(lis);
+                                    for(var u = 0; u < dictionary.length; u++)
+                                    {
+                                        var dict = document.createElement("li");
+                                        var text = document.createTextNode(dict[u].childNodes[0].nodeValue);
+                                        dict.appendChild(text);
+                                    }
+                                }
+                            }
+                        }
+                    });
            
 };
--- request.php
+++ request.php
@@ -4,7 +4,40 @@
 // respond with a value
 
 $query = $_GET['q'];
-$definition = [
+//
+print "<h3>" . strtoupper($query) . "</h3>";
+print "<p>" . $definition[$query] . "</p>";
+
+if(isset($_GET['all']) == true && isset($_GET['request?p=&all=true']) == true))
+{
+    $xmldata = '<?xml version="1.0" encoding="UTF-8"?> 
+    <entries>
+            <definition name="definition" author="John">
+            A statement of the exact meaning of a word, especially in a dictionary.
+            </defintion>
+        
+            <definition name="bar" author="Mary">
+                A place that sells alcholic beverages
+                </definition>
+            <definition name="ajax" author="Kimberly">
+                Technique which involves the use of javascript and xml (or JSON)
+                </definition>
+            <definition name="html" author="Mark">
+                The standard markup language for creating web pages and web applications.
+                </definition>
+            <definition name="css" author="Carl">
+                A style sheet language used for describing the presentation of a document written in a markup language.
+                </definition>
+            <definition name="javascript" author="Jason">
+                A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+                </definition>
+            <definition name="php" author="Paul">
+                A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+                </definition>
+    </entries>'
+} else
+{
+    $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
     "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
@@ -13,6 +46,4 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
-
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+}
